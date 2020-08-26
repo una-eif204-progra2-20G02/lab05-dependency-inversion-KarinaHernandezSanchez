8d1862ee72746ce5992490bcc61d2cee782c8eb9
KarinaHernandezSanchez <68917282+KarinaHernandezSanchez@users.noreply.github.com>
2020-08-26 14:19:07 -0600

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,19 @@
 //
 
 #include <iostream>
+#include "ISendPayment.h"
+#include "CashSender.h"
+#include "Person.h"
 
 int main()
 {
     std::cout << "Hello, Universidad Nacional!" << std::endl;
+    ISendPayment* cash = new CashSender();
+    ISendPayment* transfer = new BankTransferSender();
+    ISendPayment* check = new CheckSender();
+    Person person(  "Karina","Hernandez",402280955);
+    person.processPayment(cash);
+    person.processPayment(transfer);
+    person.processPayment(check);
     return 0;
 }
