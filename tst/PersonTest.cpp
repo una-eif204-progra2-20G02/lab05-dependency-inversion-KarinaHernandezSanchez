dcad84bfbc637fc2e6fd6807f7cb0c35fe2ae1b1
KarinaHernandezSanchez <68917282+KarinaHernandezSanchez@users.noreply.github.com>
2020-08-26 16:26:27 -0600

    PersonTest

--- tst/PersonTest.cpp
+++ tst/PersonTest.cpp
@@ -10,8 +10,18 @@
 
 TEST(PersonTestSuite, VerifyProcessPayment){
     Person person;
+    person.setFirstName("Karina");
+    person.setLastName("Hernandez");
+    person.setDocumentId(402280955);
+    ISendPayment* transfer = new BankTransferSender();
 
-    EXPECT_EQ(person.processPaymentBankTransfer(), "Sending the money by transference");
-    EXPECT_EQ(person.processPaymentCash(), "Give the money in the hands");
-    EXPECT_EQ(person.processPaymentCheck(), "Sending the check with the money");
+    EXPECT_EQ(person.processPayment(transfer), "Sending the money by transference");
+
+    ISendPayment* cash = new CashSender();
+
+    EXPECT_EQ(person.processPayment(cash), "Give the money in the hands");
+
+    ISendPayment* check = new CheckSender();
+
+    EXPECT_EQ(person.processPayment(check), "Sending the check with the money");
 }
