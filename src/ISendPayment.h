775a8de8bf59ae88e46643cac26688ffe04d77bd
KarinaHernandezSanchez <68917282+KarinaHernandezSanchez@users.noreply.github.com>
2020-08-26 13:45:36 -0600

    Interface Creation

---
+++ src/ISendPayment.h
@@ -0,0 +1,13 @@
+//
+// Created by Karina Hernandez on 26-ago.-20.
+//
+
+#ifndef MY_PROJECT_NAME_ISENDPAYMENT_H
+#define MY_PROJECT_NAME_ISENDPAYMENT_H
+
+#endif //MY_PROJECT_NAME_ISENDPAYMENT_H
+
+class ISendPayment{
+public:
+    virtual string sendPayment()=0;
+};
